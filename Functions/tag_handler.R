30df5bf99bc49c59fa0b7c03d41f625f1a222be2
ironholds <ironholds@gmail.com>
2014-11-14 12:50:00 -0500

    fix site version and mobile app misclassification bugs.

--- Functions/tag_handler.R
+++ Functions/tag_handler.R
@@ -19,8 +19,8 @@ tag_handler <- function(data){
   #Identify site
   site <- character(nrow(data))
   site[grepl(x = urls, pattern = "api.php", fixed = TRUE)] <- "App"
-  site[grepl(x = urls, pattern = "zero.", fixed = TRUE)] <- "Mobile web"
-  site[grepl(x = urls, pattern = "m.w", fixed = TRUE)] <- "Mobile web"
+  site[grepl(x = urls, pattern = "zero.", fixed = TRUE) & site == ""] <- "Mobile web"
+  site[grepl(x = urls, pattern = "m.w", fixed = TRUE) & site == ""] <- "Mobile web"
   site[site == ""] <- "Desktop"
   
   #Is it a zero request?
@@ -32,7 +32,7 @@ tag_handler <- function(data){
   mobile_site_version <- character(nrow(data))
   mobile_site_version[grepl(x = data$x_analytics, pattern = "mf-m=b", fixed = TRUE)] <- "Beta"
   mobile_site_version[grepl(x = data$x_analytics, pattern = "mf-m=a", fixed = TRUE)] <- "Alpha"
-  mobile_site_version[site == "Mobile web" & !is_zero] <- "Production"
+  mobile_site_version[site == "Mobile web" & mobile_site_version == ""] <- "Production"
   mobile_site_version[mobile_site_version == ""] <- "NA"
   
   #Identify project
